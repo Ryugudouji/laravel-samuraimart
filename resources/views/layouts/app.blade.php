61b0ef1c659545ec44e75a337c42936a7573bb48
Ryugudouji <golden83caner@yahoo.co.jp>
2024-09-14 19:46:19 +0900

    mart9章

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,64 +18,8 @@
 
     <body>
         <div id="app">
-            <!-- ナビゲーションバー -->
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-
-                    <!-- アプリ名へのリンク -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-
-                    <!-- トグルボタン（モバイル用のメニュー展開ボタン） -->
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <!-- メニュー部分 -->
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto">
-
-                            <!-- ゲストユーザー向け -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-                                    </li>
-                                @endif
-
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">登録</a>
-                                    </li>
-                                @endif
-
-                                <!-- ログインユーザー向け -->
-                                @else
-                                    <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
-
-                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                            <!-- ログアウトリンク -->
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                                ログアウト
-                                            </a>
-
-                                            <!-- ログアウト用のフォーム -->
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            @component('components.header')
+            @endcomponent
 
             <main class="py-4">
                 @yield('content')
