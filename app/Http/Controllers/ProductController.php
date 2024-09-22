10798a3713104635fb2f17b27c0836a524ed1955
Ryugudouji <golden83caner@yahoo.co.jp>
2024-09-22 14:13:53 +0900

    mart20章

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,6 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $keyword = $request->keyword;
-        dd('ここまで');
         if ($request->category !== null) {
             $products = Product::where('category_id', $request->category)->sortable()->paginate(15);
             $total_count = Product::where('category_id', $request->category)->count();
