9984003e112d7ae6b3e4ad8a8b0c54a88ec2a7f0
Ryugudouji <golden83caner@yahoo.co.jp>
2024-09-27 12:02:50 +0900

    mart30章

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -4,6 +4,7 @@ use Illuminate\Routing\Router;
 use App\Admin\Controllers\CategoryController;
 use App\Admin\Controllers\ProductController;
 use App\Admin\Controllers\MajorCategoryController;
+use App\Admin\Controllers\ShoppingCartController;
 use App\Admin\Controllers\UserController;
 
 Admin::routes();
@@ -20,5 +21,6 @@ Route::group([
     $router->resource('products', ProductController::class);
     $router->resource('major-categories', MajorCategoryController::class);
     $router->resource('users', UserController::class);
+    $router->resource('shopping-carts', ShoppingCartController::class)->only('index');
 
 });
